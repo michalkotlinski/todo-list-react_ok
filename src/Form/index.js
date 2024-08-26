291de7cc400a2b507d835dfd90b4515b6217f1b0
Michal Kotlinski <michalkotlinski@gmail.com>
2024-08-26 12:51:20 +0200

    first commit

---
+++ src/Form/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+        <input className="form__input" placeholder="Co robimy?" />
+        <button className="form__button">Dodaj zadanie </button>
+    </form>
+);
+
+export default Form;
+
