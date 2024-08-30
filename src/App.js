147431aa7bb00d1a12e6921e1a201ca6b7e532f6
michalkotlinski <michalkotlinski@gmail.com>
2024-08-30 18:52:53 +0200

    form element added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
-
 const tasks = [
-  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 1, content: "przejść na Reacta", done: true },
   { id: 2, content: "zjeść obiad", done: false },
   { id: 3, content: "wyprowadzić psa", done: true },
 ];
@@ -23,22 +22,24 @@ function App() {
 		</header>
         <section className="section">
             <header className="section__header">
-                <h2>Dodaj nowe zadanie</h2>
+                <h2 className='section__title'>Dodaj nowe zadanie</h2>
             </header>
-            <div className="section__body">
-                <form className="form js-form">
-                    <input className="form__input js-input" placeholder="Co robimy?"/>
+            <div className="section__body">    
+            <form className="form">
+            <input className="form__input" placeholder="Co robimy?"/>
                     <button className="form__button">Dodaj zadanie </button>
-                </form>
+                    </form>
             </div>
         </section>
         <section className="section">
             <header className="section__header section__header--withButtons">
                 <h2>Lista zadań</h2>
-                <div className="js-buttonsContainer buttonsArea"></div>
+                <div className="buttonsArea">
+                  <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}/>
+                </div>
             </header>
             <div className="section__body section__body--withList">
-                <ul className="js-tasks"></ul>
+                <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
             </div>
         </section>
     </main>
--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Form = () => (
     <form className="form">
         <input className="form__input" placeholder="Co robimy?" />
-        <button className="form__button">Dodaj zadanie </button>
+        <button className="form__button">Dodaj zadanie</button>
     </form>
 );
 
